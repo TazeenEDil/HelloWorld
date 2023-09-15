60cb4986d669a06e41068825a5a630010b352c1c
TazeenEDil <144116457+TazeenEDil@users.noreply.github.com>
2023-09-15 09:30:37 +0500

    Add files via upload

---
+++ .stylelintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  rules: {
+    'color-no-invalid-hex': true, // Disallow invalid hex colors
+    'font-family-no-duplicate-names': true, // Disallow duplicate font family names
+    'font-family-no-missing-generic-family-keyword': true, // Disallow missing generic font family keywords
+    'function-calc-no-unspaced-operator': true, // Disallow unspaced operators within `calc` functions
+    'string-no-newline': true, // Disallow newline characters in strings
+    'declaration-block-no-shorthand-property-overrides': true, // Disallow shorthand property overrides in declaration blocks
+  },
+};
